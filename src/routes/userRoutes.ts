0b3d5b185c3d7385570686a8176f2029252efee9
Cabdinasir64 <cabdinasira64@gmail.com>
2025-09-23 16:38:11 +0300

    feat: integrate rate-limit middleware into users routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,11 +2,12 @@ import express from 'express';
 import { getUsers, getUserById, createUser, updateUser, deleteUser } from './../Controllers/userController';
 import validateUser from './../Middleware/validateUser'
 import auth from './../Middleware/auth'
+import userRateLimit from './../Middleware/userRateLimit';
 
 const router = express.Router();
 
-router.get('/', auth, getUsers);
-router.get('/:id', auth, getUserById);
+router.get('/', auth, userRateLimit, getUsers);
+router.get('/:id', auth, userRateLimit, getUserById);
 router.post('/', auth, validateUser, createUser);
 router.put('/:id', auth, validateUser, updateUser);
 router.delete('/:id', deleteUser);
