b40321cbc1c25cb5a422c1eb1d3ce98a9bf4bfca
Cabdinasir64 <cabdinasira64@gmail.com>
2025-09-23 10:25:52 +0300

    Integrate validateUser middleware in user routes
    - Added validateUser to POST /users and PUT /users/:id
    - Ensures requests are validated before hitting controller logic
    - Improves data integrity and error handling for user endpoints

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import { getUsers, getUserById, createUser, updateUser, deleteUser } from './../Controllers/userController';
+import validateUser from './../Middleware/validateUser'
 
 const router = express.Router();
 
 router.get('/', getUsers);
 router.get('/:id', getUserById);
-router.post('/', createUser);
-router.put('/:id', updateUser);
+router.post('/', validateUser, createUser);
+router.put('/:id', validateUser, updateUser);
 router.delete('/:id', deleteUser);
 
 export default router;
