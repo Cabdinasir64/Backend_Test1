fb424755ce4dc10d66870fcc863d1086cd47cfc3
Cabdinasir64 <cabdinasira64@gmail.com>
2025-09-23 10:34:12 +0300

    Integrate auth middleware in user routes
    - Added auth middleware to GET, POST, PUT routes
    - Ensures all user-related endpoints are protected
    - Maintains validation middleware order for secure and valid data

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import { getUsers, getUserById, createUser, updateUser, deleteUser } from './../Controllers/userController';
 import validateUser from './../Middleware/validateUser'
+import auth from './../Middleware/auth'
 
 const router = express.Router();
 
-router.get('/', getUsers);
-router.get('/:id', getUserById);
-router.post('/', validateUser, createUser);
-router.put('/:id', validateUser, updateUser);
+router.get('/', auth, getUsers);
+router.get('/:id', auth, getUserById);
+router.post('/', auth, validateUser, createUser);
+router.put('/:id', auth, validateUser, updateUser);
 router.delete('/:id', deleteUser);
 
 export default router;
