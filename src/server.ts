8e549838f5dbc565a87405949ee7e5fa300201c6
Cabdinasir64 <cabdinasira64@gmail.com>
2025-09-23 16:31:42 +0300

    chore: initial server setup with express and dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import userRoutes from './routes/userRoutes';
+import authRoutes from './routes/authRoutes'
 import logger from './Middleware/logger';
 import dotenv from 'dotenv';
 
@@ -10,9 +11,10 @@ const PORT = process.env.PORT;
 
 
 app.use(express.json()); 
-app.use(logger);      
+app.use(logger);
 
 app.use('/users', userRoutes);
+app.use('/', authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
